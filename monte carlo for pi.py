7a1ecea1550edda9a8a616a40400a14e76a39527
Laurence Dugdale <laurencedugdale@outlook.com>
2019-07-07 18:40:00 +0100

    Update monte carlo for pi.py

--- monte carlo for pi.py
+++ monte carlo for pi.py
@@ -1,7 +1,8 @@
-# This program uses a Monte Carlo simulation to calculate an approximate value for pi. It works by defining a circle contained within a square, where the outside
-# of the circle makes contact with the edges of the square. Points are randomly generated within the square, and a record is made of the number of points that
-# fall within the circle compared to those that fall outside. This provides the approximate relative areas of the two shapes. The area of the square is known,
-# so the area of the cirlce can be estimated. This area can then be used to provide an approximate vaule for pi by rearranging the equation for the area of a circle.
+# This program uses a Monte Carlo simulation to calculate an approximate value for pi. It works by defining a circle contained within
+# a square, where the outside of the circle makes contact with the edges of the square. Points are randomly generated within the square,
+# and a record is made of the number of points that fall within the circle compared to those that fall outside. This provides the
+# approximate relative areas of the two shapes. The area of the square is known, so the area of the cirlce can be estimated. This area
+# can then be used to provide an approximate vaule for pi by rearranging the equation for the area of a circle.
 
 
 
